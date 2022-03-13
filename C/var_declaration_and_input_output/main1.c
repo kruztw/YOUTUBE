0fcb953ad82c5beef601c1268f1fd18d220e04f4
kruztw <kruztw@gmail.com>
2022-03-13 20:46:24 +0800

    add var_declaration_and_input_output/

---
+++ C/var_declaration_and_input_output/main1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // integer
+    // var: variable
+    int var_int = 1;
+    printf("var_int = %d\n", var_int); // decimal (十進制)
+
+    // character
+    char var_char = 'a';
+    printf("var_char = %c\n", var_char);
+
+    // 浮點數 (floating point)
+    float var_float = 1.5;
+    printf("var_float = %f\n", var_float);
+
+    // 浮點數
+    double var_double = 1.6;
+    printf("var_double = %lf\n", var_float); // long float
+
+    return 0;
+}
+
---
+++ C/var_declaration_and_input_output/main2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // integer
+    // var: variable
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d + %d = %d\n", a, b, a+b);
+
+
+    return 0;
+}
+
